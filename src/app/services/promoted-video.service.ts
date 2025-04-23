72d9282dbe82c09346c423368fcd0ffd43a76d19
Rebeca Pérez <161166821+Pro-gra-mer@users.noreply.github.com>
2025-04-23 18:16:58 +0200

    getPromotedVideos with right PromoteType

--- src/app/services/promoted-video.service.ts
+++ src/app/services/promoted-video.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment.dev';
+import { PromoteType } from '../models/PromoteType';
 
 @Injectable({ providedIn: 'root' })
 export class PromotedVideoService {
   constructor(private http: HttpClient) {}
 
   getPromotedVideos(): Observable<string[]> {
-    const params = { type: 'MAIN' };
+    const params = { type: PromoteType.MAIN_SLIDER };
     return this.http.get<string[]>(
       `${environment.apiUrl}/api/articles/promoted-videos`,
       { params }
